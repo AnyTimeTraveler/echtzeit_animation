3820532b4d32f901d2c6d75ba9f04270f55a8b94
Simon Struck <simon@simonscode.org>
2020-12-07 16:59:56 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+char animation[] =
+   R"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+X                                                                                        X
+X                                                                                        X
+X                                                                                        X
+X                                                                               | o |    X
+X                                                                               | o |    X
+X                                                                               | o |    X
+X                                                                               | o |    X
+X                                                                               | o |    X
+X                                                                               | o |    X
+X         | |         | |         | |                                           | o |    X
+X         | |         | |         | |   [X ]                                    | o |    X
+X         |o|         |o|         |o|     ||                                    | o |    X
+X         / /         / /         / /     ||            [/\]                    | o |    X
+X    --|-/ /----|----/ /----|----/ /-------------|------//\\---------|----------    |    X
+X              >         >         >         >         //  \\      >  \             |    X
+X    --|--------|-----------|--------------------|------\   \--------|--------------     X
+X                                                        \   \                           X
+X                                                         \   \                          X
+X                                                          \   \                         X
+X                                                           \   \                        X
+X                                                            \   \                       X
+X                                                             \   \                      X
+X                                                              \   \                     X
+X                                                               \   \                    X
+X                                                                                        X
+X                                                                                        X
+X                                                                                        X
+X                                                                                        X
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+int main() {
+    printf("Hello, World!\n");
+
+    return 0;
+}
